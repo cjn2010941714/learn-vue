ee45fe8c2d0c285ca6c40fe6cfe92c71f31050ac
cjn2010941714 <2010941714@qq.com>
2020-02-29 21:37:18 +0800

    vuex学习

---
+++ learnvuex/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h2>-----------------App内容-------------------</h2>
+    <h2>{{msg}}</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="add">+</button>
+    <button @click="sub">-</button>
+    <h2>-----------------HelloVuex内容-------------------</h2>
+    <hello-vuex/>
+  </div>
+</template>
+
+<script>
+  import HelloVuex from "./components/HelloVuex";
+  export default {
+    name: 'App',
+    components: {
+      HelloVuex
+    },
+    data() {
+      return {
+        msg: '我是App组件',
+        counter: 0
+      }
+    },
+    methods: {
+      add() {
+        this.$store.commit('increment')
+      },
+      sub() {
+        this.$store.commit('decrement')
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
