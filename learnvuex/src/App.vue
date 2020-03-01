e87c6333c8676fc9fafacb0f2215013ff90c9bb4
cjn2010941714 <2010941714@qq.com>
2020-03-01 23:03:03 +0800

    vuex-getter/mutation学习

--- learnvuex/src/App.vue
+++ learnvuex/src/App.vue
@@ -5,6 +5,17 @@
     <h2>{{$store.state.counter}}</h2>
     <button @click="add">+</button>
     <button @click="sub">-</button>
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+    <button @click="addStudent">添加学生</button>
+    <button @click="updateStudent">修改info信息</button>
+    <h2>-----------------App内容: info-------------------</h2
+    <h2>{{$store.state.info}}</h2>
+    <h2>-----------------App内容: getter-------------------</h2>
+    <h2>{{$store.getters.powerCounter}}</h2>
+    <h2>{{$store.getters.more18Students}}</h2>
+    <h2>{{$store.getters.more18StudentsLength}}</h2>
+    <h2>{{$store.getters.moreAgeStudents(20)}}</h2>
     <h2>-----------------HelloVuex内容-------------------</h2>
     <hello-vuex/>
   </div>
@@ -12,6 +23,7 @@
 
 <script>
   import HelloVuex from "./components/HelloVuex";
+  import {INCREMENT, DECREMENT, INCREMENTCOUNT,UPDATESTUDENT} from './store/mutations-types'
   export default {
     name: 'App',
     components: {
@@ -25,10 +37,32 @@
     },
     methods: {
       add() {
-        this.$store.commit('increment')
+        this.$store.commit(INCREMENT)
       },
       sub() {
-        this.$store.commit('decrement')
+        this.$store.commit(DECREMENT)
+      },
+      addCount(count) {
+        // payload: 负载
+        // 1.普通提交方式
+        // this.$store.commit('incrementCount',count)
+        // 2。特殊提交方式
+        this.$store.commit({
+          type: INCREMENTCOUNT,
+          count
+        })
+      },
+      addStudent() {
+        const student = {
+          id: 114,
+          name: 'xiaobai',
+          age: 25
+        }
+        this.$store.commit('addStudent',student);
+      },
+      updateStudent() {
+        // this.$store.dispatch('aUpdateInfo','我是参数')
+        this.$store.dispatch('aUpdateInfo','我是参数')
       }
     }
   }
--- learnvuex/src/store/index.js
+++ learnvuex/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from  'vue'
 import Vuex from 'vuex'
+import {INCREMENT,DECREMENT,INCREMENTCOUNT,UPDATEINFO} from './mutations-types'
 
 // 1.安装插件
 Vue.use(Vuex)
@@ -7,19 +8,57 @@ Vue.use(Vuex)
 // 2.创建对象
 const store = new Vuex.Store({
   state: {
-    counter: 1000
+    counter: 100,
+    students: [
+      {id: 110, name: 'Sky', age: 18},
+      {id: 111, name: 'xiaoming', age: 19},
+      {id: 112, name: 'xiaohong', age: 17},
+      {id: 113, name: 'xiaowang', age: 20}
+    ],
+    info: {id: 110, name: 'Sky', age: 18}
   },
   mutations: {
     // 方法
-    increment() {
+    [INCREMENT](state) {
       state.counter++
     },
-    decrement() {
+    [DECREMENT](state) {
       state.counter--
+    },
+    [INCREMENTCOUNT](state,payload) {
+      state.counter = state.counter + payload.count
+    },
+    addStudent(state,student) {
+      state.students.push(student)
+    },
+    [UPDATEINFO](state) {
+      state.info.name = 'ABC'
     }
   },
-  actions: {},
-  getters: {},
+  actions: {
+    aUpdateInfo(context,payload) {
+      setTimeout(() => {
+        console.log(payload);
+        context.commit(UPDATEINFO);
+      },1000)
+    }
+  },
+  getters: {
+    powerCounter(state) {
+      return state.counter * state.counter
+    },
+    more18Students(state) {
+      return state.students.filter(student => student.age >= 18)
+    },
+    more18StudentsLength(state,getters) {
+      return getters.more18Students.length
+    },
+    moreAgeStudents(state) {
+      return function (age) {
+        return state.students.filter(s => s.age >= 20)
+      }
+    }
+  },
   modules: {}
 });
 
